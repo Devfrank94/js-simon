7489877c451a3dde3e136d08d285dab596abb324
Devfrank94 <francescomurro94@gmail.com>
2023-03-31 15:28:25 +0200

    partial commit

--- js/script.js
+++ js/script.js
@@ -6,3 +6,21 @@
 5. Controlliamo quanti e quali dei numeri casuali sono stati indovinati dall'utente
 6. Stampiamo il risultato finale in pagina
 */
+
+// Creiamo un array vuoto per memorizzare i numeri casuali
+let randomNumbers = [];
+
+console.log('array vuoto', randomNumbers)
+
+// Generiamo 5 numeri casuali diversi e li inseriamo nell'array
+while (randomNumbers.length < 5) {
+  let randomNumber = Math.floor(Math.random() * 10);
+
+  console.log('numeri generati casualmente ->', randomNumber)
+
+  if (!randomNumbers.includes(randomNumber)) {
+    randomNumbers.push(randomNumber);
+
+    console.log('array con numeri', randomNumbers)
+  }
+}
