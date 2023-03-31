71ce5f5b086780c859c11effeba190892c53f567
Devfrank94 <francescomurro94@gmail.com>
2023-03-31 17:02:10 +0200

    partial commit

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@
 
 // Creiamo un array vuoto per memorizzare i numeri casuali
 let randomNumbers = [];
+const output = document.querySelector("h1")
 
 console.log('array vuoto', randomNumbers)
 
@@ -22,5 +23,36 @@ while (randomNumbers.length < 5) {
     randomNumbers.push(randomNumber);
 
     console.log('array con numeri', randomNumbers)
+
+    output.innerHTML = `Memorizza i numeri in 5 secondi: ${randomNumbers}
+    `
+
+    
   }
 }
+
+// Visualizziamo i numeri in pagina per 5 secondi prima che scompaiano 
+setTimeout(function() {
+
+
+  // Chiediamo all'utente di inserire i numeri che ha visto
+  let userInput = [];
+  for (let i = 0; i < 5; i++) {
+    let inputNumber = prompt("Inserisci un numero da 0 a 9");
+    userInput.push(parseInt(inputNumber));
+
+    console.log('Numeri inseriti dall\'utente',userInput)
+  }
+
+  // Controlliamo quanti e quali dei numeri casuali sono stati indovinati dall'utente
+  let correctNumbers = [];
+  for (let i = 0; i < randomNumbers.length; i++) {
+    if (userInput.includes(randomNumbers[i])) {
+      correctNumbers.push(randomNumbers[i]);
+    }
+  }
+
+  // Stampiamo il risultato finale in pagina
+  output.innerHTML = `Hai indovinato ${correctNumbers.length} numeri: ${correctNumbers}.`;  
+  
+}, 5000);
